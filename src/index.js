4583c7a650d1fb8bb6997ef25a90e6770ee6a320
Ekaterina Polyakova <epolyakova2020@mail.ru>
2026-09-20 21:32:41 +0300

    задание урока 21 выполнено

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import countTimer from './countTimer.js';
 import menu from './menu.js'; 
-import modal from './modal.js'
+import modal from './modal.js';
 
 countTimer('31 December 2026'); 
-
 menu();
-modal()
+modal();
