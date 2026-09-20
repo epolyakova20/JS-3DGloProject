9bde3bc1578f57e79b6d4ddaa893fc1387f04d44
Ekaterina Polyakova <epolyakova2020@mail.ru>
2026-09-20 20:31:54 +0300

    задание урока 20

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
